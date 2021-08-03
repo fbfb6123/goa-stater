076f28620aeee0e13651977a6341670e3bb0dbbe
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-08-03 10:44:44 +0900

    cors追記

--- design/api_definition.go
+++ design/api_definition.go
@@ -2,12 +2,21 @@ package design
 
 import (
 	. "goa.design/goa/v3/dsl"
+	cors "goa.design/plugins/v3/cors/dsl"
 	_ "goa.design/plugins/v3/zaplogger"
 )
 
 var _ = API("goa_starter", func() {
 	Title("goa_starterulator Service")
 	Description("Service for adding numbers, a Goa teaser")
+	cors.Origin("*", func() {
+		cors.Headers("X-Authorization", "X-Time", "X-Api-Version",
+			"Content-Type", "Origin", "Authorization")
+		cors.Methods("GET")
+		cors.Expose("Content-Type", "Origin")
+		cors.MaxAge(100)
+		cors.Credentials()
+	})
 	Server("goa_starter", func() {
 		Host("localhost", func() {
 			URI("http://localhost:8085/api/v1")
